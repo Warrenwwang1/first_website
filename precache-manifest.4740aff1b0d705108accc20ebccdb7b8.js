a0f3083a430c251e512bb08d78e7b67829205b5b
Warren <warrenwwang@berkeley.edu>
2019-02-17 12:42:16 -0800

    Updates

---
+++ precache-manifest.4740aff1b0d705108accc20ebccdb7b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "121194200c1b9c61afb9c9b4398cf632",
+    "url": "/static/media/app1.12119420.png"
+  },
+  {
+    "revision": "832d63ff7fee44915346",
+    "url": "/static/css/main.855b9671.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2631361b7172f3d5f672",
+    "url": "/static/js/2.2631361b.chunk.js"
+  },
+  {
+    "revision": "68692a031f19a75a1670d83e81b51ec6",
+    "url": "/static/media/piratebaby.68692a03.png"
+  },
+  {
+    "revision": "c20eeb3e4cf6dfdbcde6dcb008fd1a97",
+    "url": "/static/media/codebase_icon.c20eeb3e.png"
+  },
+  {
+    "revision": "5e48a10ece86b6423eb935ef3c0feb8c",
+    "url": "/static/media/bogosort.5e48a10e.png"
+  },
+  {
+    "revision": "832d63ff7fee44915346",
+    "url": "/static/js/main.832d63ff.chunk.js"
+  },
+  {
+    "revision": "dd41d27aba8c34b70dcb37309f65742a",
+    "url": "/static/media/app2.dd41d27a.png"
+  },
+  {
+    "revision": "34ff10aeb88057d80afe21c560c7801d",
+    "url": "/static/media/resume_icon.34ff10ae.png"
+  },
+  {
+    "revision": "db1d9969d469e6add093daa818689e6b",
+    "url": "/static/media/email_icon.db1d9969.png"
+  },
+  {
+    "revision": "650977d7450cfab31da4f2af7de979c4",
+    "url": "/static/media/github_icon.650977d7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "20b2b7463e776d2c03467ed6da60f87e",
+    "url": "/index.html"
+  }
+];
